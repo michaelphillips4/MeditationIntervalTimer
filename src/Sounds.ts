e2c8f1d9eb8b3aacb330c4163536fd9b5f5d5c20
michaelp <michaelp@linux-laptop.lan>
2025-05-28 15:05:11 +0100

    changed case of sound key to always be lower case

--- src/Sounds.ts
+++ src/Sounds.ts
@@ -2,12 +2,12 @@ import bowl from "./assets/sounds/Bowl.mp3";
 import bell from "./assets/sounds/Bell.mp3";
 
 const soundsMap = new Map<string,HTMLAudioElement>() ;
-soundsMap.set("Bell", new Audio(bell));
-soundsMap.set("Bowl", new Audio(bowl));
+soundsMap.set("bell", new Audio(bell));
+soundsMap.set("bowl", new Audio(bowl));
 
   const playSound = (sound: string) => {
     console.log("playing sound:", sound);
-    const audio = soundsMap.get(sound);
+    const audio = soundsMap.get(sound.toLocaleLowerCase());
     if (audio) {
       audio.play().catch((error) => {
         console.error("Error playing sound:", error);
