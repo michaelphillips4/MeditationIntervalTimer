cb4a1ec164a9d028655ad486eed7603b36fa5db0
michaelp <michaelp@linux-laptop.lan>
2025-06-01 18:20:51 +0100

    improved pwa offline support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,53 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
-// https://vite.dev/config/
+
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      // add this to cache all the imports
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      // add this to cache all the
+      // static assets in the public folder
+      includeAssets: ["**/*"],
+      manifest: {
+        theme_color: "#f7f7f8",
+        background_color: "#f7f7f8",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        short_name: "Meditation Timer",
+        description: "A meditation Interval Timer",
+        name: "Meditation Interval Timer",
+        icons: [
+          {
+            src: "/images/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "/images/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
