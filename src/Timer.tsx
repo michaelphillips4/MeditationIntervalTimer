a690db3099b02352dcb1dada62c5d017e595dd68
michaelp <michaelp@linux-laptop.lan>
2025-06-11 16:19:19 +0100

    Added memo to stop quote and image update on any ui changes in the timer.

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
 import type { Section, StorageMeditation } from "./Utils/typeDefinitions";
 import Meditation from "./Meditation/Meditation";
 import MeditationControls from "./Meditation/MeditationControls";
 import OpenSaveDialogButton from "./Meditation/OpenSaveDialogButton";
 import { useLocation } from "react-router-dom";
-import RandomImage from "./RandomImage";
-import RandomQuote from "./Quotes/RandomQuote";
+
 import SaveDialog from "./Meditation/SaveDialog";
+import ChildC from "./ImageAndQuote";
 
 function Timer() {
   const [currentSectionIndex, setCurrentSectionIndex] = useState<number>(-1);
@@ -28,26 +28,31 @@ function Timer() {
 
   return (
     <>
-    <main id="main">
-      <Meditation
+      <main id="main">
+        <Meditation
+          sections={sections}
+          setSections={setSections}
+          name={name}
+          currentSectionIndex={currentSectionIndex}
+          setCurrentSectionIndex={setCurrentSectionIndex}
+        />
+        <MeditationControls
+          sections={sections}
+          currentSectionIndex={currentSectionIndex}
+          setCurrentSectionIndex={setCurrentSectionIndex}
+        />
+        <OpenSaveDialogButton saved={saved} setSave={setSave} />
+        <ChildC/>
+        
+      </main>
+   
+      <SaveDialog
+        saved={saved}
+        setSaved={setSaved}
         sections={sections}
-        setSections={setSections}
-        name={name}
-        currentSectionIndex={currentSectionIndex}
-        setCurrentSectionIndex={setCurrentSectionIndex}
+        save={save}
+        setSave={setSave}
       />
-      <MeditationControls
-        sections={sections}
-        currentSectionIndex={currentSectionIndex}
-        setCurrentSectionIndex={setCurrentSectionIndex}
-      />
-      <OpenSaveDialogButton saved={saved} 
-      setSave={setSave} />
-      <RandomQuote />
-      <RandomImage />
-    </main>
-    <SaveDialog saved={saved} setSaved={setSaved} 
-      sections={sections} save={save} setSave={setSave} />
     </>
   );
 }
