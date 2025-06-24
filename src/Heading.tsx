ae9716dd18a078415380d7076660c247cdb59166
Michael Phillips <michael.phillips4@yahoo.co.uk>
2025-06-24 22:13:05 +0100

    fixed build issue

--- src/Heading.tsx
+++ src/Heading.tsx
@@ -1,11 +1,9 @@
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 import "./Heading.css";
 import { Menu } from "./Menu";
 
 const Heading = () =>
  (
-
-  
   <>
     <header>
       <h1><NavLink to="/timer">Meditation Timer</NavLink></h1>
